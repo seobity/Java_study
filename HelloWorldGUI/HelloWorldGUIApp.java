7b30c6480d9ee7ad2237c27f492942e8086715a4
seobity <codemin1010@gmail.com>
2026-07-08 01:16:15 +0900

    데스크톱 애플리케이션

---
+++ HelloWorldGUI/HelloWorldGUIApp.java
@@ -0,0 +1,21 @@
+import javax.swing.*;   
+import java.awt.Dimension;
+import java.awt.Toolkit;
+public class HelloWorldGUIApp{
+    public static void main(String[] args){
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                JFrame frame = new JFrame("HelloWorld GUI");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setPreferredSize(new Dimension(400, 300)); // 윈도우 창 크기
+                JLabel label = new JLabel("Hello World!!", SwingConstants.RIGHT);
+                frame.getContentPane().add(label);
+                Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setLocation(dim.width/2-400/2, dim.height/2-300/2);
+
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
