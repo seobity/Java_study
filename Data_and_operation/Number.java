f8c9227c39409cba32611f7dbd70a826fb987193
seobity <codemin1010@gmail.com>
2026-07-13 16:11:07 +0900

    숫자와 연산

--- Data_and_operation/Number.java
+++ Data_and_operation/Number.java
@@ -8,9 +8,9 @@ public class Number {
 		System.out.println(6 * 2); // 12
 		System.out.println(6 / 2); // 3
 		
-		System.out.println(Math.PI); // 3.141592
-		System.out.println(Math.floor(Math.PI));
-		System.out.println(Math.ceil(Math.PI));
+		System.out.println(Math.PI); // 3.141592653589793
+		System.out.println(Math.floor(Math.PI)); // 3.0
+		System.out.println(Math.ceil(Math.PI)); // 4.0
 
 	}
 
